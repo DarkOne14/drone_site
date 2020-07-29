0a2226520bc3e0db987e052eefd4f9b21aa09347
Drew - Mac <hirseljmajr@gmail.com>
2020-07-29 18:42:20 -0400

    maybe it's over

--- src/App.js
+++ src/App.js
@@ -10,18 +10,24 @@ import Footer from './Footer';
 import ImageGallery from './ImageGallery';
 import VideoGallery from './VideoGallery';
 import {Route, Link, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
+  console.log(process.env.PUBLIC_URL);
   return (
     <div className="App">
-      <Navbar />
-      <Route exact path={process.env.PUBLIC_URL + "/"} component={Home} />
-      <Route exact path={process.env.PUBLIC_URL + "/gallery"} component={Gallery} />
-      <Route exact path={process.env.PUBLIC_URL + "/what-we-do"} component={WhatWeDo} />
-      <Route exact path={process.env.PUBLIC_URL + "/contact"} component={Contact} />
-      <Route exact path={process.env.PUBLIC_URL + "/photos"} component={ImageGallery} />
-      <Route exact path={process.env.PUBLIC_URL + "/videos"}component={VideoGallery} />
-      <Footer />
+     
+      <Router basename={process.env.PUBLIC_URL}>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        {/* <Route exact path={'gallery'} component={Gallery} /> */}
+        <Route path="/what-we-do" component={WhatWeDo} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/photos" component={ImageGallery} />
+        <Route path="/videos" component={VideoGallery} />
+        <Footer />
+      </Router>
+      
     </div>
   );
 }
